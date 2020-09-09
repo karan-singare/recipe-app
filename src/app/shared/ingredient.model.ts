735e39e4ee8bbb481d1e4bdc3bb47096b2861656
Karan Singh Singare <Karan Singh Singare>
2020-09-09 23:15:09 +0530

    * created the layout for the shopping list component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient {
+  /**
+   * constructor(public name: string, public amount: number)
+   * The aboev syntax create properties  of the class with the same name as args
+   * and initialize them with the value passed to the constructor
+   */
+  constructor(public name: string, public amount: number) {}
+}
