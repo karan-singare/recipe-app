7c1ded6b8c5ed86992fe14788022505181fa0645
Karan Singh Singare <Karan Singh Singare>
2020-09-09 22:40:34 +0530

    * created the layout for the recipe section without any dynamic data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Hydrabadi Biryani', 'Recipe for the Special Biryani based on Hyderabad', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Hyderabadi_Chicken_Biryani.jpg/800px-Hyderabadi_Chicken_Biryani.jpg'),
+    new Recipe('Hydrabadi Biryani', 'Recipe for the Special Biryani based on Hyderabad', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Hyderabadi_Chicken_Biryani.jpg/800px-Hyderabadi_Chicken_Biryani.jpg'),
+  ];
 
   constructor() { }
 
